a3f0e8f2d2930c9d0b7d8355da75108d24b4619d
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2026-02-12 15:19:42 +0000

    fix: conditional remove the mixin and force the game to run with jdk 21

---
+++ src/main/java/me/ajh123/be_quiet_negotiator/MixinPlugin.java
@@ -0,0 +1,52 @@
+package me.ajh123.be_quiet_negotiator;
+
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        //? if >=1.21.2 {
+        if (mixinClassName.equals("me.ajh123.be_quiet_negotiator.mixins.CheckFeatureFlagsMixin")) {
+            return false;
+        }
+        //? }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+}
--- src/main/java/me/ajh123/be_quiet_negotiator/mixins/CheckFeatureFlagsMixin.java
+++ src/main/java/me/ajh123/be_quiet_negotiator/mixins/CheckFeatureFlagsMixin.java
@@ -1,8 +1,11 @@
 package me.ajh123.be_quiet_negotiator.mixins;
 
+import org.spongepowered.asm.mixin.Mixin;
+
+import me.ajh123.be_quiet_negotiator.BeQuietNegotiator;
+
 //? if <1.21.2 {
-/*import me.ajh123.be_quiet_negotiator.BeQuietNegotiator;
-import me.ajh123.be_quiet_negotiator.ClientConfig;
+/*import me.ajh123.be_quiet_negotiator.ClientConfig;
 import net.minecraft.network.protocol.configuration.ClientConfigurationPacketListener;
 import net.neoforged.neoforge.network.configuration.CheckFeatureFlags;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,3 +30,11 @@ public class CheckFeatureFlagsMixin {
     }
 }
 *///?}
+
+//? if >=1.21.2 {
+
+// do nothing - the mixin plugin will exclude this mixin on 1.21.2+ where the check for custom feature flags has been removed from NeoForge
+@Mixin(BeQuietNegotiator.class)
+public class CheckFeatureFlagsMixin {
+}
+//?}
